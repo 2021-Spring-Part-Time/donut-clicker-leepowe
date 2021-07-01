7753ce3eb7bf9a6fac2ed3ff7a6241e005754e87
leepowe <leepowell2@yahoo.com>
2021-07-01 19:56:01 -0400

    the functionality for the costAutoClickers increase broke two priviously successful tests

--- DonutMaker.js
+++ DonutMaker.js
@@ -1,24 +1,26 @@
 class DonutMaker {
-  constructor(numDonuts, numAutoClickers) {
+  constructor(numDonuts, numAutoClickers, costAutoClicker) {
     this.numDonuts = numDonuts;
     this.numAutoClickers = numAutoClickers;
+    this.costAutoClicker = costAutoClicker;
   }
 
   addDonut() {
     this.numDonuts++;
   }
 
-  addAutoClicker(){
-      if(this.numDonuts >= 100){
-          this.numAutoClickers++;
-          this.numDonuts -= 100;
-      }
+  addAutoClicker() {
+    if (this.numDonuts >= this.costAutoClicker) {
+      this.numAutoClickers++;
+      this.numDonuts -= this.costAutoClicker;
+      this.costAutoClicker = this.costAutoClicker / 10 + this.costAutoClicker;
+    }
   }
 
-  activateAutoClickers(){
-      if(this.numAutoClickers > 0){
-          this.numDonuts += this.numAutoClickers * 1;
-      }
+  activateAutoClickers() {
+    if (this.numAutoClickers > 0) {
+      this.numDonuts += this.numAutoClickers * 1;
+    }
   }
 }
 
--- donut-maker.test.js
+++ donut-maker.test.js
@@ -2,25 +2,30 @@ import DonutMaker from "./donutMaker";
 
 describe("the creation of DonutMaker", () => {
   test("Does it add a donut?", () => {
-    const underTest = new DonutMaker(0, 0);
+    const underTest = new DonutMaker(0, 0, 0);
     underTest.addDonut();
     expect(underTest.numDonuts).toEqual(1);
   });
 
-  test('Can it purchase an autoclicker and remove spent donuts?', ()=>{
-      const underTest = new DonutMaker(100, 0);
-      underTest.addAutoClicker();
-      expect(underTest.numAutoClickers).toEqual(1);
-      expect(underTest.numDonuts).toEqual(0);
-  })
-  test('Should not allow autoclicker purchase with 99 Donuts', ()=>{
-    const underTest = new DonutMaker(99, 0);
+  test("Can it purchase an autoclicker and remove spent donuts?", () => {
+    const underTest = new DonutMaker(100, 0, 0);
+    underTest.addAutoClicker();
+    expect(underTest.numAutoClickers).toEqual(1);
+    expect(underTest.numDonuts).toEqual(0);
+  });
+  test("Should not allow autoclicker purchase with 99 Donuts", () => {
+    const underTest = new DonutMaker(99, 0, 0);
     underTest.addAutoClicker();
     expect(underTest.numAutoClickers).toEqual(0);
-  })
-  test('Will add donuts increase by the amount of autoclickers present when fired.', ()=>{
-      const underTest = new DonutMaker(110, 2);
-      underTest.activateAutoClickers();
-      expect(underTest.numDonuts).toEqual(112);
-  })
+  });
+  test("Will add donuts increase by the amount of autoclickers present when fired.", () => {
+    const underTest = new DonutMaker(110, 2, 0);
+    underTest.activateAutoClickers();
+    expect(underTest.numDonuts).toEqual(112);
+  });
+  test("Second autoclicker will cost an additional 10%", () => {
+    const underTest = new DonutMaker(200, 1, 100);
+    underTest.addAutoClicker();
+    expect(underTest.costAutoClicker).toEqual(110);
+  });
 });
