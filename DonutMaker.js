54379ea384d0579cd83557c79e355d49c295ace3
leepowe <leepowell2@yahoo.com>
2021-07-01 23:41:07 -0400

    only need to do feature 6 on interation 2

--- DonutMaker.js
+++ DonutMaker.js
@@ -1,8 +1,10 @@
 class DonutMaker {
-  constructor(numDonuts, numAutoClickers, costAutoClicker) {
+  constructor(numDonuts, numAutoClickers, costAutoClicker, numMultipliers, costMultiplier) {
     this.numDonuts = numDonuts;
     this.numAutoClickers = numAutoClickers;
     this.costAutoClicker = costAutoClicker;
+    this.numMultipliers = numMultipliers;
+    this.costMultiplier = costMultiplier;
   }
 
   addDonut() {
@@ -21,6 +23,19 @@ class DonutMaker {
     if (this.numAutoClickers > 0) {
       this.numDonuts += this.numAutoClickers * 1;
     }
+  }
+
+  addMultiplier(){
+    if (this.numDonuts >= this.costMultiplier)
+    this.numMultipliers++;
+    this.numDonuts -= this.costMultiplier;
+    this.costMultiplier = this.costMultiplier / 10 + this.costMultiplier;
+  }
+
+  activateMultipliers() {
+      if (this.numMultipliers > 0) {
+          this.numDonuts = Math.pow(1.2, this.numMultipliers);
+      }
   }
 }
 
